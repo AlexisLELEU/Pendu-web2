49af9f0b8da12201396c725001028de62dcb960a
AlexisLELEU <alexis.leleu@hetic.net>
2017-10-30 15:10:24 +0100

    pendu fonctionnel v1.1

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var word = ['manger', 'courir', 'sport', 'programme', 'interface'];
 var letter = document.querySelector('#letter');
 var btn = document.querySelector('#button');
 var para = document.querySelector('p');
+var find = document.querySelector('h3');
 
 var random = word[Math.floor(word.length * Math.random())];
 
-var cpt = 0;
+var cpt = 1;
+var nbtent = 5;
 var nbLetter = 0;
 
 console.log(random);
@@ -17,12 +19,14 @@ console.log(random);
     btn.addEventListener('click', function (e) {
         if (cpt<5){
             if (letter.value == random[nbLetter]){
-                alert('lettre ok ');
+                letter.value = '';git
+                find.innerHTML = find.innerHTML + random[nbLetter];
                 nbLetter ++;
                 cpt++
                 console.log(random[nbLetter])
             }else{
-                alert('faux');
+                letter.value = '';
+                para.innerHTML = 'il vous reste ' + (nbtent - cpt) + ' vie';
                 cpt++;
                 console.log(random[nbLetter])
             }
